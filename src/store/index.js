4d62b2b0a608fbf26fd0f3adae9ae403af8bbc15
Md.Rezaul Karim <masum.luf@gmail.com>
2019-08-14 15:52:35 +0600

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+export const storeage = {
+  state: {
+    inventory: [],
+    cart: []
+  },
+  getters: {
+    getInventory(state) {
+      return state.inventory
+    },
+    getCart(state) {
+      return state.cart
+    }
+  },
+  mutations: {
+    // addToCart(store, payload) {
+    //   store.cart.push(payload)
+    // },
+    itemRemoved(store, payload) {
+      store.cart.splice(payload, 1)
+    },
+    checkItem(state, item) {
+      // if (state.cart.length < 1) {
+      //   state.cart.push(item)
+      // }
+      state.cart.forEach(c => {
+        if (c.id === item.id && c.lenght !== 0) {
+          console.log('item found');
+        } else {
+          state.cart.push(item)
+        }
+      })
+      //console.log(state.cart);
+      //return result
+    }
+  }
+}
